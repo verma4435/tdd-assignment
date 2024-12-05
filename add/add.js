9b373634c2489f893e5c62d34d44793db8b413ff
Amit Verma <verma4435@gmail.com>
2024-12-05 21:32:41 +0530

    test case for the -ve number strings

--- add/add.js
+++ add/add.js
@@ -5,6 +5,10 @@ exports.add = function (numStr) {
     const numStrWithoutNewLine = numStr.replace("\n", ",");
     let sum = 0;
     const numberArr = numStrWithoutNewLine.split(',').map(n => Number(n));
+    if (numberArr.some(n => n < 0)) {
+        const negativeNumberArr = numberArr.filter(n => n < 0);
+        throw new Error(`negative numbers not allowed ${negativeNumberArr}`);
+    }
     for (const num of numberArr) {
         sum += num;
     }
--- add/add.test.js
+++ add/add.test.js
@@ -6,4 +6,14 @@ describe("add function test", () => {
     test("input as 1,5", () => expect(add("1,5")).toEqual(6));
     test("input as 1,2,3,4,5,6", () => expect(add("1,2,3,4,5,6")).toEqual(1 + 2 + 3 + 4 + 5 + 6));
     test("input with new line 1\n2,3", () => expect(add("1\n2,3")).toEqual(1 + 2 + 3))
+    test(
+        "input with -ve numbers 1,2,3,-4",
+        () => expect(() => add("1,2,3,-4"))
+            .toThrow("negative numbers not allowed -4")
+    )
+    test(
+        "input with -ve numbers 1,2,3,-4,-5",
+        () => expect(() => add("1,2,3,-4,-5"))
+            .toThrow("negative numbers not allowed -4,-5")
+    )
 });
