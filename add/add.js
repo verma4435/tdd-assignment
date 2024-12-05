b3f3b3a76258d0abe0fcd6afb62fc9a744793625
Amit Verma <verma4435@gmail.com>
2024-12-05 21:14:57 +0530

    test case for the input with new line

--- add/add.js
+++ add/add.js
@@ -2,8 +2,9 @@ exports.add = function (numStr) {
     if (!numStr) {
         return 0;
     }
+    const numStrWithoutNewLine = numStr.replace("\n", ",");
     let sum = 0;
-    const numberArr = numStr.split(',').map(n => Number(n));
+    const numberArr = numStrWithoutNewLine.split(',').map(n => Number(n));
     for (const num of numberArr) {
         sum += num;
     }
--- add/add.test.js
+++ add/add.test.js
@@ -4,5 +4,6 @@ describe("add function test", () => {
     test("input as empty string", () => expect(add("")).toEqual(0));
     test("input as 1", () => expect(add("1")).toEqual(1));
     test("input as 1,5", () => expect(add("1,5")).toEqual(6));
-    test("input as 1,2,3,4,5,6", () => expect(add("1,2,3,4,5,6")).toEqual(1 + 2 + 3 + 4 + 5 + 6))
+    test("input as 1,2,3,4,5,6", () => expect(add("1,2,3,4,5,6")).toEqual(1 + 2 + 3 + 4 + 5 + 6));
+    test("input with new line 1\n2,3", () => expect(add("1\n2,3")).toEqual(1 + 2 + 3))
 });
