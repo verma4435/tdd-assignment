ae26032c8ce027bb787963fdbe4ac391a9bc8a88
Amit Verma <verma4435@gmail.com>
2024-12-05 21:05:53 +0530

    test case for 1

--- add/add.js
+++ add/add.js
@@ -2,4 +2,7 @@ exports.add = function (numStr) {
     if (!numStr) {
         return 0;
     }
+    const sum = 0;
+    const number = Number(numStr);
+    return sum + number;
 }
--- add/add.test.js
+++ add/add.test.js
@@ -2,4 +2,5 @@ const { add } = require('./add');
 
 describe("add function test", () => {
     test("input as empty string", () => expect(add("")).toEqual(0));
+    test("input as 1", () => expect(add("1")).toEqual(1));
 });
