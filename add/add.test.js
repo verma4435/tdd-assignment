ce96bd3311672cc6d42bacfe05a634d4d20fa53a
Amit Verma <verma4435@gmail.com>
2024-12-05 21:10:01 +0530

    test cases for multiple numbers

--- add/add.test.js
+++ add/add.test.js
@@ -4,4 +4,5 @@ describe("add function test", () => {
     test("input as empty string", () => expect(add("")).toEqual(0));
     test("input as 1", () => expect(add("1")).toEqual(1));
     test("input as 1,5", () => expect(add("1,5")).toEqual(6));
+    test("input as 1,2,3,4,5,6", () => expect(add("1,2,3,4,5,6")).toEqual(1 + 2 + 3 + 4 + 5 + 6))
 });
